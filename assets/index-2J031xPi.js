4375a5f73171d51c68532f35c187e22c0fbadb8f
zeeyeh <zeeyeh@163.com>
2024-01-27 15:36:46 +0800

    v1.0.1

---
+++ assets/index-2J031xPi.js
@@ -0,0 +1,1 @@
+import{o,u as r}from"./index-o81tgMgb.js";const a={__name:"index",setup(t){const e=r();return o(()=>e.replace("/info/job")),(n,u)=>null}};export{a as default};
---
+++ assets/index-a8QaK7Wk.js
@@ -0,0 +1,1 @@
+import{o as r,r as t,a as n,c as a,u as s}from"./index-o81tgMgb.js";const m={__name:"index",setup(c){const e=s();return r(()=>e.replace("/add/job")),(u,_)=>{const o=t("router-view");return n(),a(o)}}};export{m as default};
